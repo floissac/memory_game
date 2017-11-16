3575f6b12e09d147b68464ad53aed6b727c15927
William <floissac.w@gmail.com>
2017-11-16 21:26:18 +0000

    uni 9

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-console.log("Up and running!");
-// represents the cards of the game
-var cards = ['queen', 'queen', 'king', 'king'];
-// represents the cards in play
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-console.log("User flipped king")
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
+var checkForMatch = function(){
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
 } else {
-  alert('Sorry, try again.');
+console.log("Sorry, try again.");
+}
+}
+
+var flipCard = function(cardId){
+flipCard(0)
+flipCard(2)
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch()
 }
