d5e7d539f1dc40e9b3bad8854f94a903b72210a6
William <floissac.w@gmail.com>
2017-11-19 18:59:33 +0000

    final version

--- js/main.js
+++ js/main.js
@@ -1,20 +1,64 @@
-var cards = ["queen", "queen", "king", "king"];
+// CARDS
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png",
+    },
+
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png",
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png",
+    }
+];
 var cardsInPlay = [];
 
+// CHECK FOR MATCH
+var checkForMatch = function() {
 
-var checkForMatch = function(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    }
+     else {
+        alert("Sorry, try again.");
+    }
+}
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
-}
+// FLIPCARD
+var flipCard = function() {
+    var cardId = this.getAttribute("data-id");
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src',cards[cardId].cardImage);
+     if (cardsInPlay.length === 2) {
+        checkForMatch(cardsInPlay);
+    }
+};
 
-var flipCard = function(cardId){
-flipCard(0)
-flipCard(2)
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-checkForMatch()
-}
+//GAMEBOARD
+var createBoard = function () {
+    for (var i=0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+        }
+    }
+
+//INVOKE
+createBoard();
